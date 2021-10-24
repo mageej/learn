056a1199cc8b8807053b687b075749154c9b94bb
mageej <joe.agile@gmail.com>
2021-10-24 10:55:53 +0800

    first excercise

--- learn-app/src/App.svelte
+++ learn-app/src/App.svelte
@@ -1,19 +1,54 @@
 <script>
-	 let name = "Joe"; //have a variable name that can be set from outside the component
+	import ContactCard from './ContactCard.svelte';
+
+	 let name = "Joseph"; //have a variable name that can be set from outside the component
 	 let age = 58;
+	 let jobTitle;
+	 let jobDescription;
+	 let imgURL;
+
+	 //labelled statements is the base, but svelte uses this for dynamic reactive expressions
+	 // so whenever name (dependencies on the line) changes the $: expression will be run
+	$: uppercaseName = name.toUpperCase();
+
+	$: console.log(name);
+
+	$: if (name === 'Kati') { age = 58; }
 
 	 function incAge()
 	 {
 		 age += 1;
 	 }
+
+	 function changeName()
+	 {
+		 name = 'Kati';
+		 //will automatically run the $: expression
+	 }
+	 //curly braces is the core concept
+
+	 //event is passed by defeult for events
+	 function nameInput (event) {
+         const enteredValue = event.target.value;
+		 name = enteredValue;
+	 }
 </script>
 
+
 <main>
-	<h1>Hello {name}!</h1>
+	<h1>Hello {uppercaseName}</h1>
 	<p>My age is {age}</p>
 	<button on:click="{incAge}">Change Age</button> 
+	<!-- <button on:click="{changeName}">Change Name</button>  -->
+	<!-- <input type='Text' value="{name}" on:input="{nameInput}"/> -->
+	<input type='Text' bind:value="{name}" />
+	<input type='Text' aria-label="Job Title" bind:value="{jobTitle}" />
+	<input type='Text' aria-label="Job Description" bind:value="{jobDescription}" />
+
+	<ContactCard userName="{name}" jobTitle={jobTitle} jobDescription={jobDescription} />
 </main>
 
+<!-- note these styles are only scoped to this component -->
 <style>
 	main {
 		text-align: center;
@@ -24,7 +59,6 @@
 
 	h1 {
 		color: #ff3e00;
-		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
 	}
